d832cfd65e207a05fc01b14d91f6b3f219b76199
Marciano Pazinatto <marciano.et@hotmail.com>
2023-03-08 00:09:31 -0300

    updating action methods

--- src/controller/coin_action.py
+++ src/controller/coin_action.py
@@ -14,7 +14,7 @@ def validator_sames_coin(coin_one, coin_two):
 def verify_different_coin(coin_one: str, coin_two: str):
     if coin_one == coin_two:
         return validator_sames_coin(coin_one, coin_two)
-    return (coin_one, coin_two)
+    return coin_one, coin_two
 
 
 def get_coins(coin_one, coin_two):
@@ -23,26 +23,41 @@ def get_coins(coin_one, coin_two):
     other_response = requests.get(f'https://economia.awesomeapi.com.br/last/{coins[1]}-{coins[0]}')
     other_response = other_response.json()
     response = response.json()
+
     today = datetime.today().strftime('%d/%m/%Y')
-    print(response.values)
-    for i in response.values():
-        data = dict()
+    response_adjusted, other_response_adjusted = adjust_values_response(response, other_response, today)
+
+    return response_adjusted, other_response_adjusted
+
+
+def adjust_values_response(response, other_response, today):
+    response_adjusted = creating_dict(response, today)
+    other_response_adjusted = creating_dict(other_response, today)
+    return response_adjusted, other_response_adjusted
+
+
+def creating_dict(values, today):
+    data = dict()
+    if "status" in values:
+        return generate_error()
+    for i in values.values():
         data['date'] = today
-        data['ask'] = i.get('ask')
+        data['ask'] = f"{float(i.get('ask')):.2f}"
         name = i.get('name').split('/')
         data['current_name'] = name[0]
         data['compare_name'] = name[1]
-        data['high'] = i.get('high')
-        data['low'] = i.get('low')
-    print(data)
-    for i in other_response.values():
-        other_data = dict()
-        other_data['date'] = today
-        other_data['ask'] = i.get('ask')
-        name = i.get('name').split('/')
-        other_data['current_name'] = name[0]
-        other_data['compare_name'] = name[1]
-        other_data['high'] = i.get('high')
-        other_data['low'] = i.get('low')
-    print(other_data)
-    return data, other_data
+        data['high'] = f"{float(i.get('high')):.2f}"
+        data['low'] = f"{float(i.get('low')):.2f}"
+    return data
+
+
+def generate_error():
+    data = dict()
+    data['date'] = "-"
+    data['ask'] = "-"
+    name = ("-", "-")
+    data['current_name'] = name[0]
+    data['compare_name'] = name[1]
+    data['high'] = "-"
+    data['low'] = "-"
+    return data
